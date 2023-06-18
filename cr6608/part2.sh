5eeb8c7711206901891173373adc82bbbd799622
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2023-06-18 12:05:57 +0800

    Update part2.sh

--- cr6608/part2.sh
+++ cr6608/part2.sh
@@ -3,4 +3,4 @@
 # replace default theme to argon
 # rm -rf feeds/luci/themes/luci-theme-bootstrap/
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci*/Makefile
-sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
