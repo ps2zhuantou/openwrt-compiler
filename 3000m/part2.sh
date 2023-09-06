810d7064fb8dc4472e01c4cfa2da9b305e6abc35
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2023-09-06 10:28:40 +0800

    Add files via upload

---
+++ 3000m/part2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# replace default theme to argon
+# rm -rf feeds/luci/themes/luci-theme-bootstrap/
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci*/Makefile
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
